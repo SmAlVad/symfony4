f3dd3c9278182b0bcacb090a8da037d3c49e8692
Smirnov Aleksandr <smalvad@yandex.ru>
2019-06-07 11:36:00 +0900

    Ajax API

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,34 +3,44 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
 {
 
-  /**
-   *@Route("/")
-   */
-  public function index()
-  {
-    return new Response('Hello world');
-  }
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->render('base.html.twig');
+    }
 
-  /**
-   *@Route("/news/{slug}")
-   */
-  public function show($slug)
-  {
-    $comments = [
-      'lalala1',
-      'lalala2',
-      'lalala3'
-    ];
+    /**
+     * @Route("/news/{slug}", name="article_show")
+     */
+    public function show($slug)
+    {
+        $comments = [
+            'lalala1',
+            'lalala2',
+            'lalala3'
+        ];
 
-    return $this->render('article/show.html.twig', [
-      'title' => ucwords(str_replace('-', ' ', $slug)),
-      'comments' => $comments
-    ]);
-  }
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+            'slug' => $slug
+        ]);
+    }
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart($slug)
+    {
+        return $this->json(['hearts' => rand(5, 100)]);
+    }
 }
